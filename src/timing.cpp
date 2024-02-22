aed9cdf561853a62cc138200e485308866381f57
Joseph Paul <joseph@sehrgute.software>
2024-02-22 12:11:51 +0100

    Remove unneccesary header

--- src/timing.cpp
+++ src/timing.cpp
@@ -13,6 +13,14 @@ void delay_ms(const int delay) {
     usleep(delay * 1000);
 }
 
+void busy_wait_us(const int delay) {
+    usleep(delay);
+}
+
+void busy_wait_ms(const int delay) {
+    usleep(delay * 1000);
+}
+
 uint64_t uptime_ms() {
     clock_t uptime = clock();
     return (uptime * 1000) / CLOCKS_PER_SEC;
